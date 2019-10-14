9b4804171405a8ecae3785fa481a8359be52c32a
Oleh Samoilenko <oilsamoilenko@gmail.com>
2019-10-14 11:48:59 +0300

    mario is black now, no sprites for him :black_circle:
    Location: Hola Prystan'

--- OpenMario.Core/Actors/Sprites/MarioSpriteManager.cs
+++ OpenMario.Core/Actors/Sprites/MarioSpriteManager.cs
@@ -32,16 +32,6 @@ namespace OpenMario.Core.Actors.Sprites
         public const int SmallMarioHeight = 20;
 
         /// <summary>
-        /// The small mario standing left.
-        /// </summary>
-        private readonly Rectangle smallMarioStandingLeft = new Rectangle(168, 0, SmallMarioWidth, SmallMarioHeight);
-
-        /// <summary>
-        /// The small mario standing right.
-        /// </summary>
-        private readonly Rectangle smallMarioStandingRight = new Rectangle(209, 0, SmallMarioWidth, SmallMarioHeight);
-
-        /// <summary>
         /// The player.
         /// </summary>
         private readonly BasePlayer player;
@@ -68,7 +58,12 @@ namespace OpenMario.Core.Actors.Sprites
         /// </summary>
         public override void Load()
         {
-            this.fullmap = (Bitmap)Image.FromFile(@"Assets\mariosheet.png");
+            Bitmap B = new Bitmap(this.Actor.Width, this.Actor.Height);
+            for (int i = 0; i < B.Height; i++)
+                for (int j = 0; j < B.Width; j++)
+                    B.SetPixel(j, i, Color.Black);
+            this.fullmap = B;
+            this.CurrentSprite = this.fullmap;
         }
 
         /// <summary>
@@ -78,31 +73,7 @@ namespace OpenMario.Core.Actors.Sprites
         [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "Reviewed. Suppression is OK here.")]
         public override void Update(List<BaseActor> loadedactors)
         {
-            if (this.CurrentSprite == null)
-            {
-                this.CurrentSprite = new Bitmap(this.fullmap.CropImage(this.smallMarioStandingLeft), this.Actor.Width, this.Actor.Height);   
-            }
 
-            if (this.player.IsActionPressed(new KeyMapping() { Action = KeyMapping.KeyAction.LEFT }))
-            {
-                this.CurrentSprite = this.GetCrop(this.smallMarioStandingLeft);
-            }
-            else if (this.player.IsActionPressed(new KeyMapping() { Action = KeyMapping.KeyAction.RIGHT }))
-            {
-                this.CurrentSprite = this.GetCrop(this.smallMarioStandingRight);
-            }
-        }
-
-        /// <summary>
-        /// The get crop method used to crop the image to around the actor.
-        /// </summary>
-        /// <param name="crop">The <see cref="Rectangle"/> to be cropped. </param>
-        /// <returns> The <see cref="Bitmap"/> cropped. </returns>
-        private Bitmap GetCrop(Rectangle crop)
-        {
-            this.SpriteHeight = crop.Height;
-            this.SpriteWidth = crop.Width;
-            return new Bitmap(this.fullmap.CropImage(crop), this.Actor.Width, this.Actor.Height);
         }
     }
 }
